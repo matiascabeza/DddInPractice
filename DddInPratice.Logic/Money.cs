392ae11985238e7a1c6638f753928e34fba1fc55
matiascabeza <matiascabeza2012@gmail.com>
2020-07-14 16:31:01 -0300

    Se crea el proyecto DddInPratice.logic con la clase money y la clase snackmachine que agrega el money.

---
+++ DddInPratice.Logic/Money.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DddInPratice.Logic
+{
+    public sealed class Money
+    {
+        public int OneCentCount { get; private set; }
+        public int TenCentCount { get; private set; }
+        public int QuarterCount { get; private set; }
+        public int OneDollarCount { get; private set; }
+        public int FiveDollarCount { get; private set; }
+        public int TwentyDollarCount { get; private set; }
+
+        public Money(int oneCentCount, int tenCentCount, int quarterCount, int oneDollarCount, int fiveDollarCount, int twentyDollarCount)
+        {
+            OneCentCount = oneCentCount;
+            TenCentCount = tenCentCount;
+            QuarterCount = quarterCount;
+            OneDollarCount = oneDollarCount;
+            FiveDollarCount = fiveDollarCount;
+            TwentyDollarCount = twentyDollarCount;
+        }
+
+        public static Money operator +(Money money1, Money money2)
+        {
+            Money sum = new Money(
+                money2.OneCentCount + money2.OneCentCount,
+                money2.TenCentCount + money2.TenCentCount,
+                money2.QuarterCount + money2.QuarterCount,
+                money2.OneDollarCount + money2.OneDollarCount,
+                money2.FiveDollarCount + money2.FiveDollarCount,
+                money2.TwentyDollarCount + money2.TwentyDollarCount
+            );
+            return sum;
+        }
+
+
+    }
+}
