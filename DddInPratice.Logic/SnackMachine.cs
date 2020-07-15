2bc99ffbd1fe6547827e9eff8192881cadc271a2
matiascabeza <matiascabeza2012@gmail.com>
2020-07-15 12:02:03 -0300

    Se agrega la clase abstracta entity que contiene un id y metodos comparar si dos entidades son lo mismo. Se implementa la herencia entity en la clase SnackMachine.

--- DddInPratice.Logic/SnackMachine.cs
+++ DddInPratice.Logic/SnackMachine.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DddInPratice.Logic
 {
-    public sealed class SnackMachine
+    public sealed class SnackMachine : Entity
     {
         public Money MoneyInside { get; private set; }
         public Money MoneyTransaction { get; private set; }
