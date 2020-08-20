ec38104907c9ecd76ef18f291c7d7a0895329134
matiascabeza <matiascabeza2012@gmail.com>
2020-08-20 17:45:24 -0300

    Se realiza los UnitTest de snackMachine y MoneySpecs.

--- DddInPratice.Logic/Money.cs
+++ DddInPratice.Logic/Money.cs
@@ -8,12 +8,22 @@ namespace DddInPratice.Logic
 {
     public sealed class Money :ValueObject<Money>
     {
+        public static readonly Money None = new Money(0, 0, 0, 0, 0, 0);
+        public static readonly Money Cent = new Money(1, 0, 0, 0, 0, 0);
+        public static readonly Money TenCent = new Money(0, 1, 0, 0, 0, 0);
+        public static readonly Money Quarter = new Money(0, 0, 1, 0, 0, 0);
+        public static readonly Money Dollar = new Money(0, 0, 0, 1, 0, 0);
+        public static readonly Money FiveDollar = new Money(0, 0, 0, 0, 1, 0);
+        public static readonly Money TwentyDollar = new Money(0, 0, 0, 0, 0, 1);
+
         public int OneCentCount { get; private set; }
         public int TenCentCount { get; private set; }
         public int QuarterCount { get; private set; }
         public int OneDollarCount { get; private set; }
         public int FiveDollarCount { get; private set; }
         public int TwentyDollarCount { get; private set; }
+        
+        public decimal Amount =>  OneCentCount * 0.01m + TenCentCount * 0.10m + QuarterCount * 0.25m + OneDollarCount + FiveDollarCount * 5 + TwentyDollarCount * 20; 
 
         public Money(int oneCentCount, int tenCentCount, int quarterCount, int oneDollarCount, int fiveDollarCount, int twentyDollarCount)
         {
@@ -40,16 +50,28 @@ namespace DddInPratice.Logic
         public static Money operator +(Money money1, Money money2)
         {
             Money sum = new Money(
-                money2.OneCentCount + money2.OneCentCount,
-                money2.TenCentCount + money2.TenCentCount,
-                money2.QuarterCount + money2.QuarterCount,
-                money2.OneDollarCount + money2.OneDollarCount,
-                money2.FiveDollarCount + money2.FiveDollarCount,
-                money2.TwentyDollarCount + money2.TwentyDollarCount
+                money1.OneCentCount + money2.OneCentCount,
+                money1.TenCentCount + money2.TenCentCount,
+                money1.QuarterCount + money2.QuarterCount,
+                money1.OneDollarCount + money2.OneDollarCount,
+                money1.FiveDollarCount + money2.FiveDollarCount,
+                money1.TwentyDollarCount + money2.TwentyDollarCount
             );
             return sum;
         }
 
+        public static Money operator -(Money money1, Money money2)
+        {
+            Money sum = new Money(
+                money1.OneCentCount - money2.OneCentCount,
+                money1.TenCentCount - money2.TenCentCount,
+                money1.QuarterCount - money2.QuarterCount,
+                money1.OneDollarCount - money2.OneDollarCount,
+                money1.FiveDollarCount - money2.FiveDollarCount,
+                money1.TwentyDollarCount - money2.TwentyDollarCount
+            );
+            return sum;
+        }
         protected override bool EqualsCore(Money other)
         {
             return OneCentCount == other.OneCentCount &&
